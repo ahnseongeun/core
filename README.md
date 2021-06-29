a838bdb7bdc818949ef3cbcb5f6bbae9216682e5
ahnseongeun <ast3138@naver.com>
2021-06-30 01:13:14 +0900

    2021-06-30 스프링 컨테이너와 Configuration 애노테이션

--- README.md
+++ README.md
@@ -27,5 +27,19 @@ DIP, OCP, 테스트, private 생성자로 부터 자유롭게 싱글톤을 사�
 - @Configuration에서 @bean으로 등록된 객체가 다른 @bean에서 해당 bean을 호출했을 때
 bean을 호출할 때마다 new가 사용되는데 스프링은 어떻게 싱글톤 보장을 하는 것일까??
   
+- AnnotationContextApplicationContext에 파라미터로 넘긴 값은 스프링 빈으로 등록된다. 그래서
+AppConfig도 스프링 빈이 된다.
+  - 원래는 class hello.core.AppConfig가 등록되어야 하지만 xxxCGLIB가 붙으면 복잡해진다. 이렇게 된 이유는
+  내가 만든 AppConfig가 등록되는 것이 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를
+    상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것이다!
+  - 이 상속받은 임의의 클래스가 싱글톤이 보장되게 해준다.
+  - @Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고, 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환한다.
+  - AppConfig@CGLIB는 AppConfig의 자식 타입이므로, AppConfig 타입으로 조회 할 수 있다.
+  
+- 그러면 @Configuration을 적용하지 않고, @Bean만 적용하면 어떻게 될까?
+  - 그러면 우리가 만든 Class가 Bean으로 등록된다. 그러면 @Configuration은 왜 사용하는 것인가?
+  - Bean을 호출 할 때 파라미터로 Bean으로 등록된 Class를 주입할 때 new로 넣기 때문에 의존 관계가 있는 싱글톤이 깨진다.
+    - 즉, 의존 관계 관련에서 Bean을 생성 할 때 같은 새로운 new instance로 주입된다. 그리고 스프링 컨테이너가 싱글톤을 보장하지 않는다.
+  
 
     
--- src/test/java/com/spring/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/spring/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import com.spring.core.member.MemberServiceImpl;
 import com.spring.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ConfigurationSingletonTest {
@@ -20,9 +21,19 @@ public class ConfigurationSingletonTest {
 
         MemberRepository memberRepository1 = memberService.GetMemberRepository();
         System.out.println("memberRepository1 = " + memberRepository1);
+        MemberRepository memberRepository4 = memberService.GetMemberRepository();
+        System.out.println("memberRepository4 = " + memberRepository4);
         MemberRepository memberRepository2 = orderService.GetMemberRepository();
         System.out.println("memberRepository2 = " + memberRepository2);
-        Assertions.assertThat(memberRepository1).isEqualTo(memberRepository2);
+        //Assertions.assertThat(memberRepository1).isEqualTo(memberRepository2);
         System.out.println("memberRepository = " + memberRepository );
+    }
+
+    @Test
+    void configurationDeep(){
+        System.out.println(AppConfig.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
     }
 }
