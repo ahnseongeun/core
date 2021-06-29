4cb9b7a0b3fc161b40c137469651c2b2ed797f14
ahnseongeun <ast3138@naver.com>
2021-06-30 03:09:01 +0900

    2021-06-30 component filter scan

--- src/test/java/com/spring/core/scan/filter/MyExcludeComponent.java
+++ src/test/java/com/spring/core/scan/filter/MyExcludeComponent.java
@@ -5,6 +5,5 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE) //Class, interface (including annotation type), or enum declaration
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface MyIncludeComponent {
-
+public @interface MyExcludeComponent {
 }
